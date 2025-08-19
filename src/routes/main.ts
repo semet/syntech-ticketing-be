dac46f72b6f4f825dd7b673ae3708e5400ffaac7
Brian <shidoitsukaa@gmail.com>
2025-08-19 23:38:19 +0800

    t

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -3,12 +3,14 @@ import { Hono } from 'hono'
 import { AssigneeController } from '@/features/assignee/assignee-controller'
 import { CategoryController } from '@/features/category/category-controller'
 import { IssueController } from '@/features/issues/issue-controller'
+import { IssueStreamController } from '@/features/issues/issue-stream-controller'
 import { ReporterController } from '@/features/reporter/reporter-controller'
 import { WhiteLabelController } from '@/features/white-label/white-label-controller'
 
 const r = new Hono()
 
 r.get('/issues', IssueController)
+r.get('/stream/issues', IssueStreamController)
 r.get('/white-labels', WhiteLabelController)
 r.get('/category', CategoryController)
 r.get('/assignee', AssigneeController)
